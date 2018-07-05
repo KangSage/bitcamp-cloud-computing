d9b7c2b545712d5157033a14ae770056cb4309f3
KangSage <ksage@knou.ac.kr>
2018-07-05 16:37:24 +0900

    2018.07.05_16:36_HTML 페이지 추가

--- bitcamp-web-02/src/main/java/bitcamp/pms/servlet/member/MemberUpdateServlet.java
+++ bitcamp-web-02/src/main/java/bitcamp/pms/servlet/member/MemberUpdateServlet.java
@@ -17,7 +17,7 @@ import javax.servlet.http.HttpServletResponse;
 public class MemberUpdateServlet extends HttpServlet {
     
     @Override
-    protected void doGet(
+    protected void doPost(
             HttpServletRequest request, 
             HttpServletResponse response)
                     throws ServletException, IOException {

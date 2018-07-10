f2b68d4e8e36421fc2be6e9aff9692e187b093d2
KangSage <ksage@knou.ac.kr>
2018-07-10 19:01:18 +0900

    2018.07.10_19:01 jsp 전환 완료

--- bitcamp-web-04/src/main/java/bitcamp/pms/servlet/classroom/ClassroomDeleteServlet.java
+++ bitcamp-web-04/src/main/java/bitcamp/pms/servlet/classroom/ClassroomDeleteServlet.java
@@ -1,15 +1,15 @@
 package bitcamp.pms.servlet.classroom;
 
-import java.io.IOException;
-import java.io.PrintWriter;
+import bitcamp.pms.dao.ClassroomDao;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
-import bitcamp.pms.dao.ClassroomDao;
+import java.io.IOException;
+import java.io.PrintWriter;
 
 @SuppressWarnings("serial")
 @WebServlet("/classroom/delete")
@@ -20,34 +20,16 @@ public class ClassroomDeleteServlet extends HttpServlet {
             HttpServletResponse response) throws ServletException, IOException {
 
         response.setContentType("text/html;charset=UTF-8");
-        PrintWriter out = response.getWriter();
-        
-        out.println("<!DOCTYPE html>");
-        out.println("<html>");
-        out.println("<head>");
-        out.println("<meta charset='UTF-8'>");
-        out.println("<meta http-equiv='Refresh' content='1;url=list'>");
-        out.println("<title>강의 삭제</title>");
-        out.println("</head>");
-        out.println("<body>");
-        out.println("<h1>강의 삭제 결과</h1>");
-        
         try {
             ClassroomDao classroomDao = 
                     (ClassroomDao) getServletContext().getAttribute("classroomDao");
-            if (classroomDao.delete(Integer.parseInt(request.getParameter("no"))) == 0) {
-                out.println("<p>해당 강의가 없습니다.</p>");
-            } else {
-                out.println("<p>삭제하였습니다.</p>");
-            }
+            classroomDao.delete(Integer.parseInt(request.getParameter("no")));
+            response.sendRedirect("list");
+            System.out.println("delete");
         } catch (Exception e) {
-            out.println("<p>삭제 실패!</p>");
-            e.printStackTrace(out);
+            request.setAttribute("error", e);
+            RequestDispatcher rd = request.getRequestDispatcher("/error.jsp");
+            rd.forward(request, response);
         }
-        out.println("</body>");
-        out.println("</html>");
     }
-    
-
-    
 }

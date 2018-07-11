b94b1eb3f6b9e97b65a8a122dd9fb64e366ecc0f
KangSage <ksage@knou.ac.kr>
2018-07-11 11:37:49 +0900

    2018.07.11_11:37 프론트 컨트롤러 도입

---
+++ bitcamp-web-06/src/main/java/bitcamp/pms/servlet/member/MemberListServlet.java
@@ -0,0 +1,46 @@
+package bitcamp.pms.servlet.member;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import bitcamp.pms.dao.MemberDao;
+import bitcamp.pms.domain.Member;
+
+// 톰캣 서버가 호출하는 5개의 메서드
+@SuppressWarnings("serial")
+@WebServlet("/member/list")
+public class MemberListServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        
+        HashMap<String, Object> params = new HashMap<>();
+
+        if (request.getParameter("page") != null && 
+                request.getParameter("size") != null)  {
+            int page = Integer.parseInt(request.getParameter("page"));
+            int size = Integer.parseInt(request.getParameter("size"));
+            params.put("startIndex", (page - 1) * size);
+            params.put("pageSize", size);
+        }
+        
+        try {
+            MemberDao memberDao = 
+                    (MemberDao) getServletContext().getAttribute("memberDao");
+            
+            List<Member> list = memberDao.selectList(params);
+            request.setAttribute("list", list);
+            request.setAttribute("view", "/member/list.jsp");
+        } catch (Exception e) {
+            request.setAttribute("error", e);
+        }
+    }
+}

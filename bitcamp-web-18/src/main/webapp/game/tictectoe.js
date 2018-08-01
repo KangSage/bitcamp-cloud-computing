52ace84e2ab812c4a607a4990aba453e91af3400
KangSage <ksage@knou.ac.kr>
2018-08-01 21:39:26 +0900

    2018.08.01_21:39 집에가서 마저할 것

--- bitcamp-web-18/src/main/webapp/game/tictectoe.js
+++ bitcamp-web-18/src/main/webapp/game/tictectoe.js
@@ -1,15 +1,7 @@
-var one = $('#one');
-
 clickListener();
-
-$(document).off('click', 'p');
-
 function clickListener () {
     $('.firstTrTd').on('click', function (event) {
-        console.log(event.target.html());
-        if(event.target.html == 'o')
-            return;
-        $(event.target).html('x');
+        $(event.target).html('x').removeAttr();
         $(this).off('click', autoClick());
     });
 }
@@ -17,56 +9,79 @@ function clickListener () {
 
 var autoClick = function () {
     setTimeout(function () {
-        let randomNumber = 1;/*Math.floor(Math.random() * 10);*/
+        let randomNumber = 1; /*Math.floor(Math.random() * 10);*/
         if (randomNumber === 1) {
             console.log('1번 실행');
-            if (one.childElementCount > 0) {
+            if ($('#one').html() === 'o' || $('#one').html() === 'x' ) {
                 console.log('리턴전 실행');
                 return autoClick();
             } else {
-                $('#one').append('<p>o</p>');
+                $('#one').html('o');
                 clickListener();
             }
         } else if (randomNumber === 2) {
             console.log('2번 실행');
-            if ($('#two').childElementCount > 0)
+            if ($('#two').html() == 'o' ) {
+                console.log('리턴전 실행');
                 return autoClick();
-            $('#two').html('<p>o</p>');
+            } else {
+                $('#two').html('o');
+                clickListener();
+            }
         } else if (randomNumber === 3) {
             console.log('3번 실행');
-            if ($('#three').childElementCount > 0)
+            if ($('#three').html() == 'o' ) {
+                console.log('리턴전 실행');
                 return autoClick();
-            $('#three').html('<p>o</p>');
+            } else {
+                $('#three').html('o');
+                clickListener();
+            }
         } else if (randomNumber === 4) {
             console.log('4번 실행');
-            if ($('#four').childElementCount > 0)
+            if ($('#four').html() == 'o' ) {
+                console.log('리턴전 실행');
                 return autoClick();
-            $('#four').html('<p>o</p>');
+            } else {
+                $('#four').html('o');
+                clickListener();
+            }
         } else if (randomNumber === 5) {
             console.log('5번 실행');
-            if ($('#five').childElementCount > 0)
+            if ($('#five').html() == 'o' ) {
+                console.log('리턴전 실행');
                 return autoClick();
-            $('#five').html('<p>o</p>');
-        } else if (randomNumber === 6) {
-            console.log('6번 실행');
-            if ($('#six').childElementCount > 0)
-                return autoClick();
-            $('#six').html('<p>o</p>');
+            } else {
+                $('#five').html('o');
+                clickListener();
+            }
         } else if (randomNumber === 7) {
-            console.log('7번 실행');
-            if ($('#seven').childElementCount > 0)
+            console.log('1번 실행');
+            if ($('#one').html() == 'o' ) {
+                console.log('리턴전 실행');
                 return autoClick();
-            $('#seven').html('<p>o</p>');
+            } else {
+                $('#one').html('o');
+                clickListener();
+            }
         } else if (randomNumber === 8) {
-            console.log('8번 실행');
-            if ($('#eight').childElementCount > 0)
+            console.log('1번 실행');
+            if ($('#one').html() == 'o' ) {
+                console.log('리턴전 실행');
                 return autoClick();
-            $('#eight').html('<p>o</p>');
+            } else {
+                $('#one').html('o');
+                clickListener();
+            }
         } else if (randomNumber === 9) {
-            console.log('9번 실행');
-            if ($('#nine').childElementCount > 0)
+            console.log('1번 실행');
+            if ($('#one').html() == 'o' ) {
+                console.log('리턴전 실행');
                 return autoClick();
-            $('#nine').html('<p>o</p>');
+            } else {
+                $('#one').html('o');
+                clickListener();
+            }
         }
     }, 1000);
 };
